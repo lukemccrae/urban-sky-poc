7faa8b8b1228059cf29b2ae22a1693096130cfbc
lukemccrae <lukemccrae@gmail.com>
2023-12-20 11:42:32 -0600

    Initial commit

---
+++ test/urbansky-poc.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as UrbanskyPoc from '../lib/urbansky-poc-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/urbansky-poc-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new UrbanskyPoc.UrbanskyPocStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
